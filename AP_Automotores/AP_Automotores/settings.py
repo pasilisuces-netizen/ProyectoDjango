a3c3d5409a97e350a1cd586a7ebfa13a420314ca
armandopasilis <34778740+armandopasilis@users.noreply.github.com>
2026-06-20 13:48:55 -0300

    conectar proyecto con bases de datos

--- AP_Automotores/AP_Automotores/settings.py
+++ AP_Automotores/AP_Automotores/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,15 +80,14 @@ WSGI_APPLICATION = 'AP_Automotores.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'apAutomotores',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+    'default': dj_database_url.config(
+
+    default= 'postgresql://proyectodjango_35u6_user:GCd9Bi2Xo521twmgYowlGJ8tSBNTFlXK@dpg-d8rc0mj7uimc73f0n1ag-a.oregon-postgres.render.com/proyectodjango_35u6',
+    conn_max_age=600,
+    ssl_require=True
+    )
+
+   }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- AP_Automotores/AP_Automotores/urls.py
+++ AP_Automotores/AP_Automotores/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from.import create_superuser
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mi_app.urls')),
